902ed6cd855087febe8c36dd0e8d46e7b081d5fd
Allen Kim <allen.kim@rci.rogers.ca>
2018-01-24 16:24:43 -0500

    simplified store

--- store/index.js
+++ store/index.js
@@ -1,12 +1,9 @@
 export const state = () => ({
-  locales: ['en', 'fr'],
   locale: 'en'
 })
 
 export const mutations = {
   SET_LANG(state, locale) {
-    if (state.locales.indexOf(locale) !== -1) {
-      state.locale = locale
-    }
+    state.locale = locale
   }
 }
